4022a607fd529a1e78bc9355be6789885dde5c01
PalladiumGenX <grand.x.u950@gmail.com>
2024-09-01 04:42:10 +0300

    first commit3

--- app/page.tsx
+++ app/page.tsx
@@ -147,7 +147,7 @@ export default function Home() {
   }, [gameStatus]);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center lg:p-24">
       <h1 className="text-4xl font-bold mb-8">The Bug Game</h1>
       {gameStatus === "waiting" && (
         <button
